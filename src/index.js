da46d25eff56a8b302248ba2510b729f081c8b1f
Sunniva Nguyen <sunnivanguyen@gmail.com>
2023-08-24 09:46:57 +0700

    version 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//The only thing that strict mode does is that during development it will render all components twice in order to find certain bugs.
+//Also, React will check if we're using outdated parts of the React API.
